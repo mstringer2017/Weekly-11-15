b71784d32f5338731f188864ad4b14ab1d450f0c
mstringer2017 <matthewstringer@yahoo.com>
2021-11-15 17:38:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Weekly_11_15
+{
+    class Program
+    {
+        static string ReverseCase(string s)
+        {
+            char[] a = s.ToCharArray();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (Char.IsLower(a[i]))
+                    a[i] = Char.ToUpper(a[i]);
+                else if (Char.IsUpper(a[i]))
+                    a[i] = Char.ToLower(a[i]);
+            }
+            return new string (a);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseCase("Happy Birthday"));
+            Console.WriteLine(ReverseCase("MANY THANKS"));
+            Console.WriteLine(ReverseCase("sPoNtAnEoUs"));
+        }
+    }
+}
